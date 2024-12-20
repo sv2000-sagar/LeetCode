f221b9191ffad72e60d9936a7bd01c7119db0a5e
sv2000-sagar <sagarkool200032@gmail.com>
2024-12-19 23:38:16 -0800

    new

--- NC150/Python/3_Sliding Window/Minimum Window Substring.py
+++ NC150/Python/3_Sliding Window/Minimum Window Substring.py
@@ -1,7 +1,7 @@
 # Time: O(n)
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        countT, count = {}, {}
+        countT, countW = {}, {}
         for c in t:
             countT[c] = countT.get(c,0) + 1
         sIndex = 0
@@ -9,15 +9,15 @@ class Solution:
         l = 0
         length = float("infinity")
         for r in range(len(s)):
-            count[s[r]] = count.get(s[r],0) + 1
-            if(s[r] in countT and countT[s[r]] == count[s[r]]):
+            countW[s[r]] = countW.get(s[r],0) + 1
+            if(s[r] in countT and countT[s[r]] == countW[s[r]]):
                 have += 1
             while(have == need): # shrink window
-                if(r-l+1 < length):
+                if(r-l+1 < length): # new lesser window length
                     length = r-l+1
                     sIndex = l
-                count[s[l]] -=1
-                if(s[l] in countT and count[s[l]] < countT[s[l]]):
+                countW[s[l]] -=1
+                if(s[l] in countT and countW[s[l]] < countT[s[l]]):
                     have -= 1
                 l+=1
         return s[sIndex:sIndex+length] if length != float("infinity") else ""
