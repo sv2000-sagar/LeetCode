a2bdf5dc82bd44d7191578e5888d6db872d9f3c9
sv2000-sagar <sagarkool200032@gmail.com>
2024-12-19 23:12:34 -0800

    new

--- NC150/Python/2_Two Pointers/4_Container With Most Water.py
+++ NC150/Python/2_Two Pointers/4_Container With Most Water.py
@@ -5,7 +5,7 @@ class Solution:
         l,r = 0, len(height)-1
         area = 0
         while(l<r):
-            curArea = min(height[l],height[r]) * (r-l)
+            curArea = min(height[l],height[r]) * (r-l) # have to take lowest height 
             area = max(curArea,area)
             if(height[l]<height[r]): l+=1
             else: r-=1
--- NC150/Python/3_Sliding Window/3_Longest Repeating Character Replacement.py
+++ NC150/Python/3_Sliding Window/3_Longest Repeating Character Replacement.py
@@ -1,3 +1,20 @@
+# Time: O(n)
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        l = 0
+        length = 0
+        maxF = 0
+        for r in range(len(s)):
+            count[s[r]] = count.get(s[r],0) + 1
+            maxF = max(maxF, count[s[r]])
+            if(r-l+1 - maxF > k):
+                count[s[l]]-=1
+                l+=1
+            length = max(length, r-l+1)
+        return length
+
+
 # Time: O(26.n)
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
