8d31afd033cbfa1895e6e6b7bcc7f73474ad38ee
sv2000-sagar <sagarkool200032@gmail.com>
2025-01-02 23:09:06 -0800

    new

--- NC150/Python/Graphs/Pacific Atlantic Water Flow.py
+++ NC150/Python/Graphs/Pacific Atlantic Water Flow.py
@@ -1,29 +1,29 @@
-# Time: O(n*m)
-# Space: O(n*m)
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        rows,cols = len(heights), len(heights[0])
+        atl, pac = set(), set()
 
         def dfs(r,c,visited,prevH):
             if(r < 0 or r >= rows or
                c < 0 or c >= cols or
-               prevH > heights[r][c] or
-               (r,c) in visited):
-               return
-
+               (r,c) in visited or
+               prevH > heights[r][c]):
+                return 
+            
             visited.add((r,c))
-            directions = [[-1,0],[1,0],[0,1],[0,-1]]
-            for dr,dc in directions:
-                adj_r, adj_c = r+dr, c+dc
-                dfs(adj_r, adj_c, visited, heights[r][c])
+            dfs(r+1,c,visited,heights[r][c])
+            dfs(r-1,c,visited,heights[r][c])
+            dfs(r,c+1,visited,heights[r][c])
+            dfs(r,c-1,visited,heights[r][c])
 
+        # DFS from first and last row
         for c in range(cols):
-            dfs(0,c,pac,heights[0][c])
-            dfs(rows-1,c,atl,heights[rows-1][c])
+            dfs(0, c, pac, heights[0][c])  # Pacific
+            dfs(rows - 1, c, atl, heights[rows - 1][c])  # Atlantic
 
+        # DFS from first and last column
         for r in range(rows):
-            dfs(r,0,pac,heights[r][0])
-            dfs(r,cols-1,atl,heights[r][cols-1])
-        
-        return pac.intersection(atl)
+            dfs(r, 0, pac, heights[r][0])  # Pacific
+            dfs(r, cols - 1, atl, heights[r][cols - 1])  # Atlantic
+
+        return list(atl.intersection(pac))    
--- NC150/Python/Graphs/Rotting Oranges.py
+++ NC150/Python/Graphs/Rotting Oranges.py
@@ -1,35 +1,30 @@
-# Time: O(n)
-# Space: O(n)
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rows,cols = len(grid), len(grid[0])
+        rows, cols = len(grid), len(grid[0])
         fresh, time = 0,0
-        q = collections.deque()
-        visited = set()
+        q = deque([])
 
         # couting fresh and adding rottens to the queue
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh+=1
-                if grid[r][c] == 2:
+                if(grid[r][c] == 2):
                     q.append([r,c])
+                if(grid[r][c] == 1):
+                    fresh +=1
 
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
-        while(q and fresh >0):
+        directions = [[0,1],[0,-1],[1,0],[-1,0]]
+        while(q and fresh > 0):
             for i in range(len(q)):
                 r,c = q.popleft()
-                for dr,dc in directions:
-                    row,col = r+dr, c+dc
-                    if(
-                    row >= 0 and row < rows and
-                    col >= 0 and col < cols and
-                    grid[row][col] == 1 and
-                    (row,col) not in visited
+                for ar,ac in directions:
+                    row, col = r+ar, c+ac
+                    if(row >= 0 and row < rows and
+                       col >=0 and col < cols and
+                       grid[row][col] == 1
                     ):
-                        q.append([row,col])
-                        grid[row][col] == 2
-                        visited.add((row,col))
+                        grid[row][col] = 2
                         fresh-=1
+                        q.append([row,col])
             time+=1
         return time if fresh == 0 else -1
+        
