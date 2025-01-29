95190f99dfb09d5c8acfdfcd1145d206cc222b8e
sv2000-sagar <sagarkool200032@gmail.com>
2025-01-29 12:37:46 -0800

    new

--- NC150/Python/5_Binary Search/Search in Rotated Sorted Array.py
+++ NC150/Python/5_Binary Search/Search in Rotated Sorted Array.py
@@ -1,22 +1,21 @@
-# Time: O(n)
+# Time: O(logn)
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l,r = 0,len(nums)-1
+        l,r = 0, len(nums)-1
         while(l<=r):
             mid = (l+r)//2
-            if(nums[mid] == target): return mid
-
-            # left sorted part
-            if(nums[l] <= nums[mid]):
-                if(target > nums[mid] or target < nums[l]): 
-                    l = mid+1 #search right
+            if(target == nums[mid]):
+                return mid
+            # if left sorted
+            elif(nums[l] <= nums[mid]):
+                if(nums[l] <= target and nums[mid] >= target):
+                    r = mid - 1
                 else:
-                    r = mid-1
-
-            # right sorted part
+                    l = mid+1 # target is in unsorted part
+            # else right will be sorted
             else:
-                if(target < nums[mid] or target > nums[r]):
-                    r = mid-1 #search left
+                if(target >= nums[mid] and target <= nums[r]):
+                    l = mid + 1
                 else:
-                    l = mid+1 
-        return -1
+                    r = mid - 1
+        return -1      
