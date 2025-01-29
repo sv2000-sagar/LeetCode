4f362ef44de9d793e8b15206b5160759f8aa12ff
sv2000-sagar <sagarkool200032@gmail.com>
2025-01-29 12:50:30 -0800

    new

--- NC150/Python/5_Binary Search/Find Minimum In Rotated Sorted Array.py
+++ NC150/Python/5_Binary Search/Find Minimum In Rotated Sorted Array.py
@@ -1,16 +1,16 @@
 # Time: O(logn)
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        minimum = nums[0]
         l,r = 0,len(nums)-1
+        minNum = float("inf")
         while(l<=r):
             mid = (l+r)//2
-            # left sorted part
-            if(nums[mid] >= nums[l]):
-                minimum = min(minimum,nums[l])
-                l = mid+1 #search right
-            # right sorted part
-            else:
-                minimum = min(minimum,nums[mid])
-                r = mid-1 #search left
-        return minimum  
+            # if entire array is sorted (when you cross point of rotation)
+            if(nums[l] <= nums[r]):
+                return min(minNum,nums[l])
+            elif(nums[l] <= nums[mid]): # left sorted
+                minNum = min(minNum,nums[l])
+                l = mid+1
+            else: # right sorted
+                minNum = min(minNum,nums[mid])
+                r = mid-1 
